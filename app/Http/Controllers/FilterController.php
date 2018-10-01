34b8c20bb3ab1e49c69ca13e50be58e60d53a616
hanan <abdulhannanzr@gmail.com>
2018-10-01 22:16:14 +0800

    added controller and route

---
+++ app/Http/Controllers/FilterController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FilterController extends Controller
+{
+    //
+    public function filter(Request $request)
+    {
+    	
+    }
+}
